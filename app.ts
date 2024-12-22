319a69675916a2537fb52dd1b53fdc14c25fd097
naveenkumarreddyp <92851599+naveenkumarreddyp@users.noreply.github.com>
2024-12-22 11:35:54 +0530

    Update app.ts

--- app.ts
+++ app.ts
@@ -18,7 +18,7 @@ class App {
   defaultConfiguration(): void {
     this.app.use(
       cors({
-        origin: [process.env.CORS_ORIGIN_URLS!],
+        origin: [],
         credentials: true,
         methods: "GET,POST,PUT,DELETE",
         allowedHeaders: "Content-Type,Authorization",
